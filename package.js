ebcdd5bc17080d07440732d64929da0d9e8fac07
James Luck <djluck20@googlemail.com>
2015-04-07 16:20:07 +0100

    Updated package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Azure Active Directory OAuth flow",
-    version: "0.3.0",
+    version: "0.3.1",
     name: "wiseguyeh:azure-active-directory",
     git: "https://github.com/djluck/azure-active-directory"
 });
